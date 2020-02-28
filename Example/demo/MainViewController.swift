4739be4f25fc6c6a821186fb272a183cddcc7464
Laurent <laurentgdh@users.noreply.github.com>
2020-02-28 15:33:35 +0100

    update for v1.5

--- Example/demo/MainViewController.swift
+++ Example/demo/MainViewController.swift
@@ -26,13 +26,22 @@ class MainViewController: UIViewController {
     
     var accounts : [ELTAccount]
     
+    var appLevelProcessing : Bool
+    
     init() {
         self.accounts = []
+        // self.appLevelProcessing == true -> App layer processing
+        // self.appLevelProcessing == true -> SDK layer processing
+        self.appLevelProcessing = true
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func switchProcessing(control: UISwitch) {
+        self.appLevelProcessing = control.isOn
     }
     
     override func viewDidLoad() {
@@ -44,6 +53,17 @@ class MainViewController: UIViewController {
             self.title = "Remote Example " + version
         }
         
+        let switchView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.width(), height: 70))
+        let switchLabel = UILabel(frame: CGRect(x: 20, y: 20, width: UIScreen.width() - 80, height: 40))
+        switchLabel.text = "App layer processing:"
+        switchLabel.font = UIFont.systemFont(ofSize: 20)
+        switchView.addSubview(switchLabel)
+        let switchControl = UISwitch(frame: CGRect(x: UIScreen.width() - 70, y: 20, width: 50, height: 40))
+        switchControl.setOn(self.appLevelProcessing, animated: false)
+        switchControl.addTarget(self, action: #selector(switchProcessing(control:)), for: .valueChanged)
+        switchView.addSubview(switchControl)
+        self.tableView.tableHeaderView = switchView
+        
         self.tableView.registerClass(AccountTableViewCell.self)
         self.tableView.backgroundColor = .clear
         self.view.addSubview(self.tableView)
@@ -53,23 +73,6 @@ class MainViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Enroll", style: .done, target: self, action: #selector(MainViewController.enroll))
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Sign In", style: .done, target: self, action: #selector(MainViewController.signIn))
-    }
-    
-    @objc func enroll() {
-        self.showTextField(title: "Enter your name", placeholder: "Name", submitButtonTitle: "Enroll") { (text) in
-            let acc : ELTAccount = ELTAccount.createNewAccount(withFirstName: text, lastName: "")
-            let enrollmentVC = RemoteFaceEnrollmentViewController(firstName: acc.firstName, lastName: acc.lastName, userId: acc.userId, successBlock: { (vc) in
-                _ = vc.navigationController?.popViewController(animated: true)
-                DispatchQueue.main.async {
-                    acc.save()
-                    self.reloadData()
-                }
-            }, onCancel: { (vc) in
-                _ = vc.navigationController?.popViewController(animated: true)
-            })
-            enrollmentVC?.showGazeInstructions = true
-            self.navigationController?.pushViewController(enrollmentVC!, animated: true)
-        }
     }
     
     @objc func signIn() {
@@ -130,7 +133,9 @@ class MainViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    func sdkLevelAuthentication(userId: String) {
+    // MARK: Authentication
+    
+    func sdkLevelAuthenticationViewController(userId: String) -> RemoteFaceAuthenticationViewController? {
         // authenticate the user
         let sdkLevelAuthVc = RemoteFaceAuthenticationViewController(userId: userId, onAuthentication: { (viewController, confidenceScore, message) in
             print("success authenticating ", userId)
@@ -139,12 +144,104 @@ class MainViewController: UIViewController {
             print("success authenticating ", userId)
             _ = viewController.navigationController?.popViewController(animated: true)
         })
-        sdkLevelAuthVc?.showGazeInstructions = true
-        self.navigationController?.pushViewController(sdkLevelAuthVc!, animated: true)
+        return sdkLevelAuthVc
+    }
+    
+    func appLayerAuthenticationViewController(userId: String) -> RemoteFaceAuthenticationViewController? {
+        let vc = RemoteFaceAuthenticationViewController(asyncVerifyBlock: {
+            images, latitude, longitude, callback in
+            // you may pass in additional parameters
+            let result = ElementRemoteProcessingTransaction.verifyUserId(userId, with: images, latitude: latitude, longitude: longitude, additionalParameters: nil)
+            callback(result)
+        }, onAuthentication: {
+            viewController, confidenceScore in
+            print("success authenticating")
+            _ = viewController.navigationController?.popViewController(animated: true)
+        }, onCancel: {
+            viewController in
+            print("authentication cancelled")
+            _ = viewController.navigationController?.popViewController(animated: true)
+        })
+        return vc
     }
     
     func handleTap(_ account: ELTAccount) {
-        self.sdkLevelAuthentication(userId: account.userId)
+        let vc : RemoteFaceAuthenticationViewController?
+        if self.appLevelProcessing {
+            vc = self.appLayerAuthenticationViewController(userId: account.userId)
+        } else {
+            vc = self.sdkLevelAuthenticationViewController(userId: account.userId)
+        }
+        // show instructions before capture or not
+        vc?.showGazeInstructions = false
+        // show authentication success message
+        vc?.showAuthenticationSuccessScreen = true
+        self.navigationController?.pushViewController(vc!, animated: true)
+    }
+    
+    // MARK: Enrollment
+    
+    func sdkLevelEnrollmentViewController(account: ELTAccount) -> RemoteFaceEnrollmentViewController? {
+        let enrollmentVC = RemoteFaceEnrollmentViewController(firstName: account.firstName, lastName: account.lastName, userId: account.userId, successBlock: { (vc) in
+            _ = vc.navigationController?.popViewController(animated: true)
+            DispatchQueue.main.async {
+                account.save()
+                self.reloadData()
+            }
+        }, onCancel: { (vc) in
+            _ = vc.navigationController?.popViewController(animated: true)
+        })
+        return enrollmentVC
+    }
+    
+    func appLayerEnrollmentViewController(account: ELTAccount) -> RemoteFaceEnrollmentViewController? {
+        let enrollmentVC = RemoteFaceEnrollmentViewController(successBlock: { (viewController, images) in
+            _ = viewController.navigationController?.popViewController(animated: true)
+            LoadingController.showLoading("Enrolling...")
+            ElementRemoteProcessingTransaction.enrollUser(withFirstName: account.firstName, lastName: account.lastName, userId: account.userId, images: images, additionalParameters: nil, successBlock: { (resp) in
+                print(resp)
+                LoadingController.hideLoading()
+                DispatchQueue.main.async {
+                    self.showMessage(title: "Account created", message: nil, block: {
+
+                    })
+                }
+            }, errorBlock: { (error) in
+                print(error)
+                LoadingController.hideLoading()
+                DispatchQueue.main.async {
+                    self.showMessage(title: "An error happened", message: nil, block: {
+
+                    })
+                }
+            })
+        }, onCancel: { (viewController) in
+            _ = viewController.navigationController?.popViewController(animated: true)
+        })
+        return enrollmentVC
+    }
+    
+    @objc func enroll() {
+        self.showTextField(title: "Enter your name", placeholder: "Name", submitButtonTitle: "Enroll") { (text) in
+            let acc : ELTAccount = ELTAccount.createNewAccount(withFirstName: text, lastName: "")
+            let enrollmentVC : RemoteFaceEnrollmentViewController?
+            if self.appLevelProcessing {
+                enrollmentVC = self.appLayerEnrollmentViewController(account: acc)
+            } else {
+                enrollmentVC = self.sdkLevelEnrollmentViewController(account: acc)
+            }
+            // show instructions before capture or not
+            enrollmentVC?.showGazeInstructions = false
+            // 4 images enrollment
+            enrollmentVC?.enrollmentType = .normal
+            // enrollment success message
+            enrollmentVC?.enrollmentMessageForAccountBlock = {
+                return "Enrolled!"
+            }
+            // show the enrollment success popup
+            enrollmentVC?.showEnrollmentSuccessScreen = true
+            self.navigationController?.pushViewController(enrollmentVC!, animated: true)
+        }
     }
 #endif
 }
