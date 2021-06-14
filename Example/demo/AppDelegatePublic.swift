413bf5b0dea0f04ce8845e6fc7f81c5284474df5
Edy Cu <edy.cu@metranet.co.id>
2021-06-14 16:50:09 +0700

    Update EAK File

--- Example/demo/AppDelegatePublic.swift
+++ Example/demo/AppDelegatePublic.swift
@@ -25,7 +25,7 @@ class AppDelegatePublic: UIResponder, UIApplicationDelegate {
         
 #if !(targetEnvironment(simulator))
         // TODO: replace YOUR_EAK with the EAK provided by Element
-        ElementSDKConfiguration.shared().initialize(withConfigurationData: "YOUR_EAK")
+        ElementSDKConfiguration.shared().initialize(withConfigurationData: "EREREV/lCqP9INPjZByL6KSjG+uYdKV6WwAsSjLXG5ctMHNEKYPBVMwtNhWGZlRUgMp8iEVezFElNTycSqHYYZlsYAw2xTWkUs9w1o4b5bdQKbTqqHPPHouNM70zlo+OA2Upvx+sIgh3UnYI18N4E022cAiKGr8Fo26MBQ7/260yrsED+mFsJM5kxY2yb/lgbwPMiS4/2G4YBAvwq5NQd7X2roiR2Gz1O2qXVH/2iuvRBXn6Mz/S5G17Ox1LUpOWs53RD0uHAYlHX+EFZmbUvoPHZ0/gGPn2asCBMbaKkTKevUjn+gNSlYY4W81lybVQu+pyS+o3V/7hh27nsHn4FwK3+vSaPGO2AsXCJ2qFsyMyO4Thv9dv5yHEW4vVwadXMoJJyqe1g4/HNEIU8jkOP4JRbeWi+DlbREXOhXw2d84Vdscp14gqk+oSiDZN4NS2cdAU5DtP1GROD0wUUR9S4dFjFWbn11zAucnzcUWKZI0o22/55zYqsn9QYzWdrvdjygWFFMbxzVGuem1wtFtNz4BdDojJll9AVVESIBIJqZGuhXiF9dOmOdGIHH1u4ISk8ePo1D5qM+NWTcAOaz+Y8n+X1OYCzD2cA9d39c1FqUHnMAfVf9+lllUVh4YdVf0UrZ76GHdBVNIO5MxdBszgxXacKkgsi/0/qtOt8yNXG6F7IaDwQcf/j3+6Q5kJo9v1KcHLpttcZLBCFO8oJfr4cAOX8p+IA3Licfp7nvYwziMJ+zdYcujPt4hqlsZGn6F2d7WqMU0y0LhFpsH/SWRDw/cZNGUCyGVdqsYvdVCJWVJmZAHR4gDzDxePl9KvHoquzn4ZJKFbg1W4R7BB6tM4thgd2DSAjHvGvwSG5+gcEhGri2yDg8bCWQfnb/K9IE35tniM/OZdZUYq7SIulbM0r2EmyDYA8fwtDFLepGnoQcuXEUzgLd2IXUJAJwSYw0gmCKdt444XwrBdEEx1F/vZsUaphrQDdmG+VxWn7hSxk1Z8XgVSswwwsQ9/4hnrgcpuhqjTihci7/XFUksXDBX7I8Y/qYTvWVqUtA==")
         
         // theme: requires the asset bundles
         ElementSDKConfiguration.shared().theme = .flowerPetals
